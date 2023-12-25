e74547c72fa5f3da8d979c6b83182046d526bb24
ilmi's <zulilmiihsn@gmail.com>
2023-12-25 09:09:30 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // other configurations
+    theme: {
+      extend: {
+        fontFamily: {
+          'playfair': ['Playfair Display', 'serif'],
+          'roboto': ['Roboto', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [
+      
+    ],
+  }
+  
