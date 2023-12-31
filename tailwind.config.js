6842f5008763ead944a416f00d6b06ef18df1aff
ilmi's <zulilmiihsn@gmail.com>
2023-12-31 22:10:00 +0800

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // other configurations
+  theme: {
+    extend: {
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    
+  ],
+}
